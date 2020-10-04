ba824061d8ba42641ddea4cbf5805c4f09fb42cb
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2020-10-04 15:09:15 -0700

    Initial file upload

---
+++ escape.js
@@ -0,0 +1,7 @@
+// This is the page that will open on the current window, not the new tab.
+var pageReplace = "https://en.wikipedia.org/wiki/Kimchi"
+
+function escapeButton(){
+    open(pageReplace, "_self");
+    
+};
